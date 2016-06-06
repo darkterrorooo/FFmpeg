578f7d9c11f10e2a9fd648349529f59e642672ea
darkterrorooo <597326493@qq.com>
2016-06-06 10:36:38 +0800

    test
    test

--- ffmpeg.h
+++ ffmpeg.h
@@ -15,7 +15,7 @@
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
+//ljg test
 #ifndef FFMPEG_H
 #define FFMPEG_H
 
